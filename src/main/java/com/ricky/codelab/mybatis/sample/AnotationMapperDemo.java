ac7087d0ef0ae828cbf2acd4dc76bd360b35d3a9
FBing <vricky.feng@gmail.com>
2016-04-26 15:49:11 +0800

    add annotation usage

--- src/main/java/com/ricky/codelab/mybatis/sample/AnotationMapperDemo.java
+++ src/main/java/com/ricky/codelab/mybatis/sample/AnotationMapperDemo.java
@@ -6,28 +6,26 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.ricky.codelab.mybatis.sample.mapper.AuthorMapper;
-import com.ricky.codelab.mybatis.sample.model.Author;
+import com.ricky.codelab.mybatis.sample.mapper.IAuthorDao;
 
-public class AuthorMapperDemo {
+public class AnotationMapperDemo {
 
 	public static void main(String[] args) {
 		
 		String resource = "mybatis-config.xml";
 		
 		//使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        InputStream is = AuthorMapperDemo.class.getClassLoader().getResourceAsStream(resource);
+        InputStream is = AnotationMapperDemo.class.getClassLoader().getResourceAsStream(resource);
         //构建sqlSession的工厂
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-
+        
+        // 由于使用了注解，所以在主配置文件没有mapper，需要在代码里显示注册该mapper接口
+        sessionFactory.getConfiguration().addMapper(IAuthorDao.class);
+        
         SqlSession session = sessionFactory.openSession();
         try{
-        	Author author = (Author) session.selectOne("com.ricky.codelab.mybatis.sample.mapper.AuthorMapper.selectAuthorById", 1);
-            System.out.println(author);
-            
-            AuthorMapper authorMapper = session.getMapper(AuthorMapper.class);
-            author = authorMapper.selectAuthorById(2);
-            System.out.println(author);
+            IAuthorDao authorDao = session.getMapper(IAuthorDao.class);
+            System.out.println(authorDao.retrieveAllUsers());
             
         }finally{
             session.close();
