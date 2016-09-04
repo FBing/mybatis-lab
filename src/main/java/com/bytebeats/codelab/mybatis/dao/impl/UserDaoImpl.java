88096611f2b32445db3aa8a5285c1c8cfadf2aa5
Ricky Fung <ricky_feng@163.com>
2016-09-04 23:56:59 +0800

    refactor code

--- src/main/java/com/bytebeats/codelab/mybatis/dao/impl/UserDaoImpl.java
+++ src/main/java/com/bytebeats/codelab/mybatis/dao/impl/UserDaoImpl.java
@@ -1,6 +1,6 @@
 package com.bytebeats.codelab.mybatis.dao.impl;
 
-import com.bytebeats.codelab.mybatis.dao.IBaseDao;
+import com.bytebeats.codelab.mybatis.dao.SqlSessionDao;
 import com.bytebeats.codelab.mybatis.dao.IUserDao;
 import com.bytebeats.codelab.mybatis.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,41 +18,41 @@ import java.util.List;
 public class UserDaoImpl implements IUserDao {
 
     @Autowired
-    private IBaseDao baseDao;
+    private SqlSessionDao sqlSessionDao;
 
     @Override
     public long insert(User user) {
 
-        return baseDao.getSqlSession().insert("com.bytebeats.codelab.mybatis.mapper.UserMapper.insert", user);
+        return sqlSessionDao.insert("com.bytebeats.codelab.mybatis.mapper.UserMapper.insert", user);
     }
 
     @Override
     public int insertBatch(List<User> list) {
 
-        return baseDao.getSqlSession().insert("com.bytebeats.codelab.mybatis.mapper.UserMapper.batchInsert", list);
+        return sqlSessionDao.insert("com.bytebeats.codelab.mybatis.mapper.UserMapper.batchInsert", list);
     }
 
     @Override
     public int update(User user) {
 
-        return baseDao.getSqlSession().update("com.bytebeats.codelab.mybatis.mapper.UserMapper.updateById", user);
+        return sqlSessionDao.update("com.bytebeats.codelab.mybatis.mapper.UserMapper.updateById", user);
     }
 
     @Override
     public int delete(long id) {
 
-        return baseDao.getSqlSession().delete("com.bytebeats.codelab.mybatis.mapper.UserMapper.deleteById", id);
+        return sqlSessionDao.delete("com.bytebeats.codelab.mybatis.mapper.UserMapper.deleteById", id);
     }
 
     @Override
     public User queryById(long id) {
 
-        return baseDao.getSqlSession().selectOne("com.bytebeats.codelab.mybatis.mapper.UserMapper.selectById", id);
+        return sqlSessionDao.selectOne("com.bytebeats.codelab.mybatis.mapper.UserMapper.selectById", id);
     }
 
     @Override
     public List<User> queryUsers() {
 
-        return baseDao.getSqlSession().selectList("com.bytebeats.codelab.mybatis.mapper.UserMapper.selectList");
+        return sqlSessionDao.selectList("com.bytebeats.codelab.mybatis.mapper.UserMapper.selectList");
     }
 }
